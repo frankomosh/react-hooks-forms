bb02906e9e41e758c0a16e4f2a4c9ffdf721c8a9
Frank-197 <frankomosh197@gmail.com>
2022-07-25 00:42:37 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,9 @@
-// import React, { useState } from "react";
-
-// function Form() {
-//   const [firstName, setFirstName] = useState("John");
-//   const [lastName, setLastName] = useState("Henry");
-
-//   return (
-//     <form>
-//       <input type="text" value={firstName} />
-//       <input type="text" value={lastName} />
-//       <button type="submit">Submit</button>
-//     </form>
-//   );
-// }
-
-// export default Form;
-// import React, { useState } from "react";
-
-// function Form() {
-//   const [firstName, setFirstName] = useState("John");
-//   const [lastName, setLastName] = useState("Henry");
-// src/components/Form
 import React from "react";
 
 function Form(props) {
   return (
     <form>
-      {/* <input type="text" value={firstName} />
-      <input type="text" value={lastName} /> */}
-      <input
+       <input
         type="text"
         onChange={props.handleFirstNameChange}
         value={props.firstName}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Form from './components/Form'
 
-// ReactDOM.render(
-//   <Form />,
-//   document.getElementById('root')
-// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
